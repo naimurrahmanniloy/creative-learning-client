5e7147469a4c03be0b6381274cc42ab4e65bd1c3
naimurrahmanniloy <niloyrahman432@gmail.com>
2022-10-25 09:28:46 +0600

    adding assets

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAo8JhCYmETqBh-xIXXEJwpbUPoGvamdJM",
+    authDomain: "creative-learning-2b905.firebaseapp.com",
+    projectId: "creative-learning-2b905",
+    storageBucket: "creative-learning-2b905.appspot.com",
+    messagingSenderId: "1018228450955",
+    appId: "1:1018228450955:web:56cf828b96726047a7168e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
